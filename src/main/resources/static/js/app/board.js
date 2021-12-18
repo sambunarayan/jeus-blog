a1462e8e0b5bd24aea8732b24e21b4480c6f5250
sambunarayan <soyou30257@gmail.com>
2021-12-18 19:37:17 +0900

    20211218 Build jeus-blog web
    ・Fixed paging handling

--- src/main/resources/static/js/app/board.js
+++ src/main/resources/static/js/app/board.js
@@ -1,19 +1,26 @@
 $(document).ready(function(){
+    list();
+    function list() {
+        var regForm = $("#hidden_form");
+        var tableBody = $("#tbody");
+        var name = document.getElementById("board_name").innerHTML;
+        var name2 = $(this).data("board_name");
+        var name3 = '${board_name}';
 
-        showList();
-        function showList() {
+        alert(name);
+        alert(name2);
+        alert(name3);
+        alert(regForm.find('#hidden_board_name').val());
 
-            var tableBody = $("#tbody");
-            alert(tableBody);
-            var body = "";
+        var body = "";
 
-            body += "<tr id='tr id'>";
-            body += "<td>1</td>";
-            body += "<td><a id='1' href='/it/board/list/kubernetes?bno=1'>test</a></td>";
-            body += "<td>a</td>";
-            body += "<td>b</td>";
-            body += "</tr>";
-            alert(body);
-            tableBody.html(body);
-        }
+        body += "<tr id='tr id'>";
+        body += "<td>1</td>";
+        body += "<td><a id='1' href='/it/board/list/kubernetes?bno=1'>test</a></td>";
+        body += "<td>a</td>";
+        body += "<td>b</td>";
+        body += "</tr>";
+        alert(body);
+        tableBody.html(body);
+    }
 });
