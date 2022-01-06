6476f3571b1005f3c41e609b046da5b424a5c31b
sambunarayan <soyou30257@gmail.com>
2022-01-06 22:18:20 +0900

    20220106 Build jeus-blog web
    ・Add test cases for the controller.

---
+++ src/test/java/jp/co/jeus/blog/web/ITBulletinBoardControllerTest.java
@@ -0,0 +1,31 @@
+package jp.co.jeus.blog.web;
+
+import org.junit.Before;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ITBulletinBoardControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void findAll() throws Exception {
+        mvc.perform(get("/it/board/bulletin"))
+                .andExpect(status().isOk());
+    }
+}
