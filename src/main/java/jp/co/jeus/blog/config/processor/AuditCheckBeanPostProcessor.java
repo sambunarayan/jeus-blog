85fd095ca07716500d80ce1749d8162c06700167
sambunarayan <soyou30257@gmail.com>
2022-02-03 08:20:08 +0900

    20220203 Build jeus-blog
    ・Added BeanPostProcessor

---
+++ src/main/java/jp/co/jeus/blog/config/processor/AuditCheckBeanPostProcessor.java
@@ -0,0 +1,23 @@
+package jp.co.jeus.blog.config.processor;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.stereotype.Component;
+
+@Log4j2
+@Component
+public class AuditCheckBeanPostProcessor implements BeanPostProcessor {
+
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        log.debug("In AuditCheckBeanPostProcessor.postProcessBeforeInitialization," + "processing bean type:" + bean.getClass());
+        return bean;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        log.debug("In AuditCheckBeanPostProcessor.postProcessAfterInitialization," + "processing bean type:" + bean.getClass());
+        return bean;
+    }
+}
