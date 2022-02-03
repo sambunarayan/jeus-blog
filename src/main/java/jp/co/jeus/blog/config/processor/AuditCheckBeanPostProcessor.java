d6c12139cbd6039dee6594946995fa2c011bc1c5
sambunarayan <soyou30257@gmail.com>
2022-02-04 08:57:42 +0900

    20220204 Build jeus-blog
    ・Fixed log rolling policy

--- src/main/java/jp/co/jeus/blog/config/processor/AuditCheckBeanPostProcessor.java
+++ src/main/java/jp/co/jeus/blog/config/processor/AuditCheckBeanPostProcessor.java
@@ -3,6 +3,7 @@ package jp.co.jeus.blog.config.processor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
 
 @Log4j2
@@ -11,13 +12,17 @@ public class AuditCheckBeanPostProcessor implements BeanPostProcessor {
 
     @Override
     public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
-        log.debug("In AuditCheckBeanPostProcessor.postProcessBeforeInitialization," + "processing bean type:" + bean.getClass());
+        if (bean instanceof JpaRepository) {
+            log.debug("In AuditCheckBeanPostProcessor.postProcessBeforeInitialization," + "processing bean type:" + bean.getClass());
+        }
         return bean;
     }
 
     @Override
     public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
-        log.debug("In AuditCheckBeanPostProcessor.postProcessAfterInitialization," + "processing bean type:" + bean.getClass());
+        if (bean instanceof JpaRepository) {
+            log.debug("In AuditCheckBeanPostProcessor.postProcessAfterInitialization," + "processing bean type:" + bean.getClass());
+        }
         return bean;
     }
 }
