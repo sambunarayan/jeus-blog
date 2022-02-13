9c57854afc83901e569b8bc1fef4176cd8b190f1
sambunarayan <soyou30257@gmail.com>
2022-02-14 08:55:21 +0900

    20220214 Build jeus-blog
    Add thumbnailator

--- src/main/java/jp/co/jeus/blog/service/ImageUploadService.java
+++ src/main/java/jp/co/jeus/blog/service/ImageUploadService.java
@@ -1,12 +1,14 @@
 package jp.co.jeus.blog.service;
 
 import lombok.extern.log4j.Log4j2;
+import net.coobird.thumbnailator.Thumbnailator;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.util.UUID;
 
 @Log4j2
@@ -17,18 +19,19 @@ public class ImageUploadService {
     @Value("${logo.image.filepath:}")
     private String logoPath;
 
-    public String uploadLogImage(MultipartFile logoFiles) {
-        if (logoFiles == null) {
+    public String uploadLogImage(MultipartFile logoFile) {
+        if (logoFile == null) {
             return "";
         }
         UUID logoFileName = UUID.randomUUID();
-        log.debug("Logo original file name : " + logoFiles.getOriginalFilename());
-        log.debug("Logo fileSize : " + logoFiles.getSize());
+        log.debug("Logo original file name : " + logoFile.getOriginalFilename());
+        log.debug("Logo fileSize : " + logoFile.getSize());
         log.debug("New logo file name : " + logoFileName.toString());
 
         File saveFile = new File(logoPath, logoFileName.toString());
-        try {
-            logoFiles.transferTo(saveFile);
+        try (FileOutputStream thumbnail = new FileOutputStream(saveFile);) {
+//            logoFiles.transferTo(saveFile);
+            Thumbnailator.createThumbnail(logoFile.getInputStream(), thumbnail, 250, 250);
         } catch (Exception e) {
             log.error(e.getMessage());
         }
