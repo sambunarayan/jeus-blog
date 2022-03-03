6d8accecacf34f7ee922c71570cbf9ddad5b6711
sambunarayan <soyou30257@gmail.com>
2022-03-04 08:50:56 +0900

    20220304 Build jeus-blog
    ・Fixed JUnit test case

--- src/test/java/jp/co/jeus/blog/web/ITBulletinBoardRestControllerTest.java
+++ src/test/java/jp/co/jeus/blog/web/ITBulletinBoardRestControllerTest.java
@@ -1,14 +1,19 @@
 package jp.co.jeus.blog.web;
 
-import com.google.gson.Gson;
+import com.google.gson.*;
+import com.google.gson.reflect.TypeToken;
 import jp.co.jeus.blog.web.dto.PostResponseDto;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 
+import java.lang.reflect.Type;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -29,7 +34,13 @@ public class ITBulletinBoardRestControllerTest {
                 .andReturn()
                 .getResponse()
                 .getContentAsString();
-//        List<PostResponseDto> resList = new Gson().fromJson(result, PostResponseDto.class);
-        System.out.println(result);
+        Gson gson = new GsonBuilder().registerTypeAdapter(LocalDateTime.class, new JsonDeserializer<LocalDateTime>() {
+            @Override
+            public LocalDateTime deserialize(JsonElement json, Type type, JsonDeserializationContext jsonDeserializationContext) throws JsonParseException {
+                return LocalDateTime.parse(json.getAsJsonPrimitive().getAsString());
+            }
+        }).create();
+        List<PostResponseDto> resList = gson.fromJson(result, new TypeToken<ArrayList<PostResponseDto>>(){}.getType());
+        resList.forEach(System.out::println);
     }
 }
