c0528cb9012176e12556852a4b068cfd9de42cf4
sambunarayan <soyou30257@gmail.com>
2022-03-12 22:50:08 +0900

    20220312 Build jeus-blog
    ・Add Unit test case for Image upload service.

---
+++ src/test/java/jp/co/jeus/blog/service/ImageUploadServiceTest.java
@@ -0,0 +1,21 @@
+package jp.co.jeus.blog.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ImageUploadServiceTest {
+
+    @Autowired
+    private ImageUploadService service;
+
+//    @Test
+    private void uploadLogImage() {
+        String logoImageName = service.uploadLogImage(null);
+        assertThat(logoImageName).isNull();
+    }
+}
