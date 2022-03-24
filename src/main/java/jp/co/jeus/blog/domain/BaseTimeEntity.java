7e471f9189cfd72d4f32b06f005096c0e4b09a35
sambunarayan <soyou30257@gmail.com>
2022-03-24 23:16:29 +0900

    20220324 Build jeus-blog
    ・Added comments to Auditing entity class

--- src/main/java/jp/co/jeus/blog/domain/BaseTimeEntity.java
+++ src/main/java/jp/co/jeus/blog/domain/BaseTimeEntity.java
@@ -4,22 +4,29 @@ import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
 import javax.persistence.Column;
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
-
+/**
+ * This is AuditingEntityClass that automatically puts the created date and modified date.
+ */
 @Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseTimeEntity {
 
+    /**
+     * Created date
+     */
     @CreatedDate
     @Column(name = "created_date")
     private LocalDateTime createdDate;
 
+    /**
+     * Modified date
+     */
     @LastModifiedDate
     @Column(name = "modified_date")
     private LocalDateTime modifiedDate;
