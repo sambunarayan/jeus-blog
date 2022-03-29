3e8f3adccf9bc26c3cfde609b1a9a191098be7e4
sambunarayan <soyou30257@gmail.com>
2022-03-29 22:25:54 +0900

    20220329 Build jeus-blog
    ・Fixed posting js

--- src/main/java/jp/co/jeus/blog/web/ImageRestController.java
+++ src/main/java/jp/co/jeus/blog/web/ImageRestController.java
@@ -3,10 +3,7 @@ package jp.co.jeus.blog.web;
 import jp.co.jeus.blog.service.ImageUploadService;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @Log4j2
@@ -17,11 +14,10 @@ public class ImageRestController {
     @Autowired
     private ImageUploadService imageUploadService;
 
-
-    @RequestMapping(value="/upload", method= RequestMethod.POST, produces="*/*")
+    @RequestMapping(value="/upload", method= RequestMethod.POST, produces="application/json")
     @ResponseBody
-    public String uploadImage(MultipartFile multipartFile) {
-        log.debug(multipartFile.getOriginalFilename());
-        return imageUploadService.uploadLogoImage(multipartFile);
+    public String uploadImage(MultipartFile file) {
+        log.debug(file.getOriginalFilename());
+        return imageUploadService.uploadLogoImage(file);
     }
 }
--- src/main/resources/static/js/app/posting.js
+++ src/main/resources/static/js/app/posting.js
@@ -1,23 +1,24 @@
 function onChange(evt) {
     var files = evt.target.files;
+    var data = new FormData();
     for (var i = 0, f; f = files[i]; i++) {
         // Code to execute for every file selected
         alert(files[i].name);
-//        var data = new FormData(files[i]);
-        $.ajax({
-            type: 'POST',
-            enctype: 'multipart/form-data',
-            url: '/app/v1/RestImage/upload',
-            processData: false,
-            contentType: 'multipart/form-data',
-            cache: false,
-            timeout: 600000,
-            data: files[i],
-        }).done(function(data) {
-            alert(data);
-        }).fail(function (error) {
-            alert(error);
-        });
+//        data.append('files['+i+']', files[i]);
     }
+    data.append("file", files[0]);
+    $.ajax({
+        type: 'POST',
+        url: '/app/v1/RestImage/upload',
+        processData: false,
+        contentType: false,
+        cache: false,
+        timeout: 600000,
+        data: data,
+    }).done(function(data) {
+        console(data);
+    }).fail(function (error) {
+        alert(error);
+    });
 }
 document.getElementById("uploadFile").addEventListener("change", onChange, false);
