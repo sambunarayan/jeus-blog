ad9eed28583d499201dd53b9c0428a49e4b65230
sambunarayan <soyou30257@gmail.com>
2022-04-12 23:19:52 +0900

    20220412 Build jeus-blog
    Add login form

---
+++ src/main/java/jp/co/jeus/blog/web/AccountController.java
@@ -0,0 +1,17 @@
+package jp.co.jeus.blog.web;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequiredArgsConstructor
+@RequestMapping("accounts")
+@Controller
+public class AccountController {
+
+    @GetMapping("login")
+    public String login() {
+        return "login";
+    }
+}
