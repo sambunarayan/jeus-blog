a08abb74952db2875817417304d86dc93e3a380b
sambunarayan <soyou30257@gmail.com>
2022-04-19 20:39:54 +0900

    20220419 Build jeus-blog
    ・Add User table entity

--- src/main/java/jp/co/jeus/blog/domain/posts/Users.java
+++ src/main/java/jp/co/jeus/blog/domain/posts/Users.java
@@ -1,17 +1,27 @@
 package jp.co.jeus.blog.domain.posts;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Getter
 @NoArgsConstructor
 @Entity
 public class Users {
 
+    @Id
     private String userId;
     private String password;
     private String userName;
-    private String gmail;
+    private String mail;
+
+    @Builder
+    public Users(String userId, String password, String userName, String mail) {
+        this.userId = userId;
+        this.password = password;
+        this.userName = userName;
+        this.mail = mail;
+    }
 }
