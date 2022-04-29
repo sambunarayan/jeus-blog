0c5978c0ccb2d9aeb5d3134c50c349834db45942
sambunarayan <soyou30257@gmail.com>
2022-04-29 22:25:43 +0900

    20220429 Build jeus-blog

--- src/main/java/jp/co/jeus/blog/domain/posts/Users.java
+++ src/main/java/jp/co/jeus/blog/domain/posts/Users.java
@@ -7,6 +7,9 @@ import lombok.NoArgsConstructor;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
+/**
+ * Users entity
+ */
 @Getter
 @NoArgsConstructor
 @Entity
--- src/main/java/jp/co/jeus/blog/domain/posts/UsersRepository.java
+++ src/main/java/jp/co/jeus/blog/domain/posts/UsersRepository.java
@@ -2,6 +2,9 @@ package jp.co.jeus.blog.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/**
+ * Users repository
+ */
 public interface UsersRepository extends JpaRepository<Users, Long> {
 
 }
