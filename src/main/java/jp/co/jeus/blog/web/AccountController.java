686a9e21ccda29a3e1d13d0a127d4870f8666ba9
sambunarayan <soyou30257@gmail.com>
2022-05-04 14:30:30 +0900

    20220504 Build jeus-blog
    ・Fixed login controller

--- src/main/java/jp/co/jeus/blog/web/AccountController.java
+++ src/main/java/jp/co/jeus/blog/web/AccountController.java
@@ -3,6 +3,7 @@ package jp.co.jeus.blog.web;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequiredArgsConstructor
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class AccountController {
 
-    @GetMapping("login")
+    @PostMapping("login")
     public String login() {
         return "login";
     }
