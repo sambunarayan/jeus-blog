c6917234465a55c8a3a9d45a9f4ef04acc5c69a2
sambunarayan <soyou30257@gmail.com>
2022-07-07 21:25:18 +0900

    Fixed

--- src/main/java/jp/co/jeus/blog/web/JspTestController.java
+++ src/main/java/jp/co/jeus/blog/web/JspTestController.java
@@ -1,6 +1,5 @@
 package jp.co.jeus.blog.web;
 
-import lombok.extern.log4j.Log4j;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
