4c9f54ca5463b58ced55389c42d923a9cc2accba
sambunarayan <soyou30257@gmail.com>
2022-07-14 21:02:39 +0900

    Delete deprecated api

--- src/main/java/jp/co/jeus/blog/config/ServletConfig.java
+++ src/main/java/jp/co/jeus/blog/config/ServletConfig.java
@@ -9,7 +9,6 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
-import org.springframework.web.servlet.view.ResourceBundleViewResolver;
 
 /**
  * ServletConfig
@@ -27,13 +26,6 @@ public class ServletConfig implements WebMvcConfigurer {
     public MultipartResolver multipartResolver() {
         StandardServletMultipartResolver resolver = new StandardServletMultipartResolver();
         return resolver;
-    }
-
-    @Bean
-    public ViewResolver resourceBundleViewResolver() {
-        ResourceBundleViewResolver bean = new ResourceBundleViewResolver();
-        bean.setBasename("views");
-        return bean;
     }
 
     /**
