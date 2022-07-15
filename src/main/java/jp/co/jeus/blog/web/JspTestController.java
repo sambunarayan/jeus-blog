1c61d8cdc00ba22ab12a7797fb58ab6a96602dd0
sambunarayan <soyou30257@gmail.com>
2022-07-15 22:36:45 +0900

    Add comment

--- src/main/java/jp/co/jeus/blog/web/JspTestController.java
+++ src/main/java/jp/co/jeus/blog/web/JspTestController.java
@@ -4,6 +4,9 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+/**
+ * Controller for JSP Test
+ */
 @Log4j2
 @Controller
 public class JspTestController {
