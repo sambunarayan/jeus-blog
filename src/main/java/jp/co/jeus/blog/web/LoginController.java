952f719f96cf989ca3d888136abcff512dd6f07b
sambunarayan <soyou30257@gmail.com>
2022-07-20 22:14:17 +0900

    Delete unecessary import

--- src/main/java/jp/co/jeus/blog/web/LoginController.java
+++ src/main/java/jp/co/jeus/blog/web/LoginController.java
@@ -3,7 +3,6 @@ package jp.co.jeus.blog.web;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * Login Test page
